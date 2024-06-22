57c6c7503143d0f66f75f10eeecce90518f41065
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-06-22 04:59:11 +0300

    ADD exercise-four in Lesson-11

---
+++ exercises/lesson-11/scripts/exercise-four.js
@@ -0,0 +1,20 @@
+for (let i = 0; i <= 10; i += 2) {
+  console.log(i);
+}
+
+let i = 0;
+while (i <= 10) {
+  console.log(i);
+  i += 2;
+}
+
+
+for (let i = 5; i >= 0; i--) {
+  console.log(i);
+}
+
+let j = 5;
+while (j >= 0) {
+  console.log(j);
+  j--;
+}
