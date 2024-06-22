90fd4e15df9f6420a2b081f432f5c50846db67f3
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-06-22 04:25:49 +0300

    UPD To-Do-List Project

--- projects/to-do-list/scripts/to-do-list.js
+++ projects/to-do-list/scripts/to-do-list.js
@@ -1,5 +1,25 @@
 const toDoList = [];
 
+function generateHTMLParagraphs() {
+  let htmlParagraphs = '';
+
+  for (let i = 0; i < toDoList.length; i++) {
+    htmlParagraphs += `<p>${toDoList[i]}</p>`
+  }
+
+  return htmlParagraphs;
+}
+
+function renderToDoList() {
+  const containerElement = document.querySelector('.js-to-do-list-container');
+
+  if (!containerElement) {
+    return;
+  }
+
+  containerElement.innerHTML = generateHTMLParagraphs();
+}
+
 function addToDo() {
   const inputElement = document.querySelector('.js-name-input');
 
@@ -9,8 +29,13 @@ function addToDo() {
 
   const name = inputElement.value;
   toDoList.push(name);
-
-  console.log(toDoList);
-
   inputElement.value = '';
+
+  renderToDoList();
+}
+
+function handleKeyDownInput(event) {
+  if (event && event.key == 'Enter') {
+    addToDo();
+  }
 }
