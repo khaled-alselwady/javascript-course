9c231f31eb87f7e7f04fcc59b9353a24f91d3eeb
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-06-22 07:00:53 +0300

    UPD exercise-nine in Lesson-11

--- exercises/lesson-11/scripts/exercise-nine.js
+++ exercises/lesson-11/scripts/exercise-nine.js
@@ -1,5 +1,5 @@
 function removeAllEgg(foods) {
-  if (!foods) {
+  if (!foods && foods.length < 1) {
     return [];
   }
 
@@ -34,7 +34,44 @@ function removeFirstTwoEggs(foods) {
   return result;
 }
 
+function reverseArray(array) {
+  if (!array && array.length < 1) {
+    return [];
+  }
+
+  const reverseArray = [];
+  const arrayLegnth = array.length;
+
+  for (let i = arrayLegnth - 1; i >= 0; i--) {
+    reverseArray[arrayLegnth - i - 1] = array[i];
+  }
+
+  return reverseArray;
+}
+
+function removeLastTwoEggs(foods) {
+  if (!foods) {
+    return [];
+  }
+
+  const result = [];
+  let eggsRemoved = 0;
+
+  for (let i = foods.length - 1; i >= 0; i--) {
+    if (foods[i] !== 'egg' || eggsRemoved >= 2) {
+      result.push(foods[i]);
+    }
+    else {
+      eggsRemoved++;
+    }
+  }
+
+  //return result.reverse();
+  return reverseArray(result);
+}
+
 const foods = ['egg', 'apple', 'egg', 'egg', 'ham', 'egg'];
 console.log(foods);
 console.log(removeAllEgg(foods));
 console.log(removeFirstTwoEggs(foods));
+console.log(removeLastTwoEggs(foods));
