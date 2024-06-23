65d40b57fa91ddd008ff5f3bf53c0979ca6c05fa
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-06-23 23:48:40 +0300

    UPD exercise-four in Lesson-12

--- exercises/lesson-12/scripts/exercise-four.js
+++ exercises/lesson-12/scripts/exercise-four.js
@@ -1,12 +1,22 @@
+let messages = 2;
+
 function changeTitle() {
   setInterval(() => {
     if (document.title === 'App') {
-      document.title = '(2) New messages';
+      document.title = `(${messages}) New messages`;
     }
     else {
       document.title = 'App';
     }
   }, 1000);
 };
+
+function addMessage() {
+  messages++;
+}
+
+function removeMessage() {
+  messages--;
+}
 
 changeTitle();
