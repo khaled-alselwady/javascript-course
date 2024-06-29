cf32e63bc75cd3cbd4df938c3a364e4decdc79ca
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-06-30 01:55:06 +0300

    UPD exercise-one in Lesson-15

--- exercises/lesson-15/scripts/exercise-one.js
+++ exercises/lesson-15/scripts/exercise-one.js
@@ -3,3 +3,6 @@ import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 const today = dayjs();
 const dateAfterFiveDays = today.add(5, 'days');
 console.log(dateAfterFiveDays.format('MMMM D'));
+
+const dateAfterOneMonth = today.add(1, 'month');
+console.log(dateAfterOneMonth.format('MMMM D'));
