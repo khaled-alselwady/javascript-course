b4d874b326338778c705f8d6cf3de5052a438d2d
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-07-04 06:34:32 +0300

    ADD exercise-one in Lesson-17

---
+++ exercises/lesson-17/scripts/car.js
@@ -0,0 +1,25 @@
+class Car {
+  brand;
+  model;
+
+  constructor(carDetails) {
+    if (!carDetails) {
+      return;
+    }
+
+    this.brand = carDetails.brand;
+    this.model = carDetails.model;
+  }
+}
+
+const car1 = new Car({
+  brand: 'Toyota',
+  model: 'Corolla'
+});
+const car2 = new Car({
+  brand: 'Tesla',
+  model: 'Model 3'
+});
+
+console.log(car1);
+console.log(car2);
